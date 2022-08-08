235938fb2441e0bf9748698daa2f557cda96bf94
Sergey Postnikov <ser.post2017@gmail.com>
2022-08-08 15:01:54 +0300

    'initial_commit'

---
+++ weather.py
@@ -0,0 +1,10 @@
+import requests
+
+
+url = 'https://ru.wttr.in/'
+options = '?n?3?M?q?T'
+
+
+for city in ('Лондон', 'Шереметьево', 'Череповец'):
+	response = requests.get(url + f'/{city}{options}')
+	print(response.text)
