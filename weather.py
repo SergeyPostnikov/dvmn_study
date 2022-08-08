6aec6d88d895f55b868af98a26f66dd29963f4f3
Sergey Postnikov <ser.post2017@gmail.com>
2022-08-08 15:04:58 +0300

    tabs to spaces

--- weather.py
+++ weather.py
@@ -6,5 +6,5 @@ options = '?n?3?M?q?T'
 
 
 for city in ('Лондон', 'Шереметьево', 'Череповец'):
-	response = requests.get(url + f'/{city}{options}')
-	print(response.text)
+    response = requests.get(url + f'/{city}{options}')
+    print(response.text)
