6bbb157f47ddccf1e9141652c593b4cdd6d2a33f
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-08-11 17:00:36 +0300

    wrapper deleted

--- weather.py
+++ weather.py
@@ -1,25 +1,22 @@
 import requests
 
-def get_weather(url, options, city):
-    response = requests.get(f'{url}/{city}', params=options)
-    response.raise_for_status()
-    print(response.text)
-
 
 def main():
     cities = ('Лондон', 'Шереметьево', 'Череповец')
     url = 'https://wttr.in/'
     options = {
         "lang": "ru",
-        "n": "", # узкая версия (только день и ночь)
-        "M": "", # показывать скорость ветра в м/с
-        "q": "", # тихая версия (без текста "Прогноз погоды")
-        "T": "", # отключить терминальные последовательности (без цветов) 
+        "n": "", 
+        "M": "", 
+        "q": "", 
+        "T": "",  
     }
 
     for city in cities:
-        get_weather(url, options, city)
-        
+        response = requests.get(f'{url}/{city}', params=options)
+        response.raise_for_status()
+        print(response.text)
+    
 
 
 if __name__ == '__main__':
