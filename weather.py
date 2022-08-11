d9036613e1908d0655c8b4f2bd032c95b55c27cd
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-08-11 15:45:12 +0300

    ifmain fixed etc

--- weather.py
+++ weather.py
@@ -1,23 +1,25 @@
 import requests
 
-
-url = 'https://wttr.in/'
-options = {
-    "lang": "ru",
-    "n": "",
-    "3": "",
-    "M": "",
-    "q": "",
-    "T": "",
-} 
+def get_weather(url, options, city):
+    response = requests.get(f'{url}/{city}', params=options)
+    response.raise_for_status()
+    print(response.text)
 
 
+def main():
+    cities = ('Лондон', 'Шереметьево', 'Череповец')
+    url = 'https://wttr.in/'
+    options = {
+        "lang": "ru",
+        "n": "", # узкая версия (только день и ночь)
+        "M": "", # показывать скорость ветра в м/с
+        "q": "", # тихая версия (без текста "Прогноз погоды")
+        "T": "", # отключить терминальные последовательности (без цветов) 
+    }
 
-def main(cities=('Лондон', 'Шереметьево', 'Череповец')):
     for city in cities:
-        response = requests.get(f'{url}/{city}', params=options)
-        response.raise_for_status()
-        print(response.text)
+        get_weather(url, options, city)
+        
 
 
 if __name__ == '__main__':
